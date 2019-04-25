31f00490245e86064fa10ac3deb396f55829c250
dwyfrequency <jdwy215@gmail.com>
2019-04-25 16:47:13 -0400

    adding routes for form

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ const app = express();
 const views = require('./views/main');
 const models = require('./models'); // do not need to specify index.js - see answer https://stackoverflow.com/questions/5364928/node-js-require-all-files-in-a-folder
 app.use(morgan('dev'));
-const userRouter = require('./routes/user');
-const wikiRouter = require('./routes/wiki');
-app.use('/user', userRouter);
-app.use('/wiki', wikiRouter);
+
 //app.use('/', views);
-const PORT = 1337;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
-//app.use(express.json());
+app.use(express.json({ extended: false }));
+
+app.use('/user', require('./routes/user'));
+app.use('/wiki', require('./routes/wiki'));
 
 app.get('/', (req, res) => {
-  res.send(views());
+  res.redirect('/');
 });
+
+// app.get('/', (req, res) => {
+//   res.send(views());
+// });
 
 models.db.authenticate().then(() => {
   console.log('CONNECTED TO DB');
@@ -28,6 +31,7 @@ const init = async () => {
   // await models.User.sync();
   // await models.Page.sync();
 
+  const PORT = 1337;
   app.listen(PORT, () => {
     console.log(`APP LISTENING ON ${PORT}`);
   });
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const addPage = require('../views/addPage');
+const {
+  addPage,
+  editPage,
+  main,
+  userList,
+  userPages,
+  wikiPage,
+} = require('../views');
 
-
-router.get("/", (req,res) => {
-  res.redirect('/');
+router.get('/', (req, res) => {
+  res.send(main());
 });
 
-router.post("/", (req, res, next) => {
-  res.json(req.body)
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  const body = req.body;
+  res.json(req.body);
   //res.send('req stuff');
-})
+});
 
 router.get('/add', (req, res, next) => {
   res.send(addPage());
