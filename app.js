5449eaf61565d77537bcc9f6c567d8e9430cee49
sam-peach <sampeachart@gmail.com>
2019-04-25 12:16:01 -0400

    db check

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-
+const views = require("./views/main");
+const { db } = require("./models");
 app.use(morgan("dev"));
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("HELLO WORLD!");
+  res.send(views());
+});
+
+db.authenticate().then(() => {
+  console.log("CONNECTED TO DB");
 });
 
 const port = 1337;
